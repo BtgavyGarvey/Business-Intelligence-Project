c14449ea8ec3ef4dbfab7e658e47bccbe3cb4823
Meshack Owino <94436554+BtgavyGarvey@users.noreply.github.com>
2023-11-29 08:45:17 +0300

    done issues in milestone 4

--- Descriptive-Statistics.R
+++ Descriptive-Statistics.R
@@ -24,7 +24,7 @@ dailySales <- read_csv("data/saleshourly.csv",
                      N05B = col_double(), N05C = col_double(), 
                      R03 = col_double(), R06 = col_double(), 
                      Year = col_number(), Month = col_number(), 
-                     Hour = col_number(), `WeekdayName` = col_factor())
+                     Hour = col_number(), `Weekday Name` = col_factor())
 )
 
 View(dailySales)
@@ -121,4 +121,159 @@ gg_miss_upset(dailySales)
 
 
 missmap(dailySales, col = c("red", "grey"), legend = TRUE)
+
+## klaR ----
+if (require("klaR")) {
+  require("klaR")
+} else {
+  install.packages("klaR", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+if (require("LiblineaR")) {
+  require("LiblineaR")
+} else {
+  install.packages("LiblineaR", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## e1071 ----
+if (require("e1071")) {
+  require("e1071")
+} else {
+  install.packages("e1071", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## 1. Split the dataset ====
+
+train_index <- createDataPartition(dailySales$M01AB,
+                                   p = 0.75,
+                                   list = FALSE)
+dailySales_train <- dailySales[train_index, ]
+dailySales_test <- dailySales[-train_index, ]
+print(dailySales_train)
+print(dailySales_test)
+
+## 1. Bootstrapping ====
+
+train_control <- trainControl(method = "boot", number = 500)
+
+
+## 1. Cross validation (repeated) ====
+
+train_control <- trainControl(method = "repeatedcv", number = 5, repeats = 3)
+
+## 1. Model Training ====
+
+demand_forecasting_dataset_model_lm <- # nolint
+  caret::train(M01AB ~
+                 datum +
+                 Year + Month + Hour + `Weekday Name`,
+               data = dailySales_train,
+               trControl = train_control,
+               na.action = na.omit, method = "lm", metric = "RMSE")
+
+predictions_lm <- predict(demand_forecasting_dataset_model_lm,
+                          dailySales_test[, 1:13])
+
+print(demand_forecasting_dataset_model_lm)
+print(predictions_lm)
+
+if (require("kernlab")) {
+  require("kernlab")
+} else {
+  install.packages("kernlab", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+## randomForest ----
+if (require("randomForest")) {
+  require("randomForest")
+} else {
+  install.packages("randomForest", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+if (require("RRF")) {
+  require("RRF")
+} else {
+  install.packages("RRF", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+## 3.a. Train the Models ----
+# We train the following models, all of which are using 10-fold repeated cross
+# validation with 3 repeats:
+#   LDA
+#   CART
+#   KNN
+#   SVM
+#   Random Fores
+
+
+### KNN ----
+set.seed(7)
+dailySales_model_knn <- train(M01AB ~ ., data = dailySales,
+                            method = "knn", trControl = train_control)
+
+
+### Random Forest ----
+set.seed(7)
+dailySales_model_rf <- train(M01AB ~ ., data = dailySales,
+                           method = "rf", trControl = train_control)
+
+## 3.b. Call the `resamples` Function ----
+# We then create a list of the model results and pass the list as an argument
+# to the `resamples` function.
+
+results <- resamples(list(
+                          KNN = dailySales_model_knn, SVM = dailySales_model_svm,
+                          RF = dailySales_model_rf))
+
+# STEP 4. Display the Results ----
+## 1. Table Summary ----
+# This is the simplest comparison. It creates a table with one model per row
+# and its corresponding evaluation metrics displayed per column.
+
+summary(results)
+
+
+# STEP 5. Apply a "Grid Search" to identify the best parameter value ----
+## RRF ----
+
+
+
+train_control <- trainControl(method = "repeatedcv", number = 10, repeats = 3,
+                              search = "grid")
+set.seed(7)
+
+getModelInfo("RRFglobal")
+
+tunegrid <- expand.grid(.mtry = c(1:10),
+                        .coefReg = seq(from = 0.1, to = 1, by = 0.1))
+
+seed <- 7
+metric <- "RMSE"
+
+dailySales_model_grid_search_rrf_global <- train(M01AB ~ ., data = dailySales, # nolint
+                                            method = "RRFglobal",
+                                            metric = metric,
+                                            tuneGrid = tunegrid,
+                                            trControl = train_control)
+print(dailySales_model_grid_search_rrf_global)
+plot(dailySales_model_grid_search_rrf_global)
+
+if (require("plumber")) {
+  require("plumber")
+} else {
+  install.packages("plumber", dependencies = TRUE,
+                   repos = "https://cloud.r-project.org")
+}
+
+predict_sales <- function(start_date, end_date, drug) {
+  dates <- seq(as.Date(start_date), as.Date(end_date), by = 'day')
+  df_test <- data.frame(datum = dates, Year = year(dates), Month = month(dates), `Weekday Name` = wday(dates), day = day(dates), Drug = drug)
+  df_test$predicted_quantity <- predict(loaded_model, newdata = as.matrix(df_test))
+  return(df_test)
+}
 
